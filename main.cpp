f722cdfebf077384568d0ea6a26bc2806b20cede
Andrew Zaikov <andrew.zaikov@gmail.com>
2023-12-24 15:05:18 +0700

    Базовые методы GLUT

--- main.cpp
+++ main.cpp
@@ -1,6 +1,75 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <GLUT/glut.h>
 
-int main(int argc, char **argv){
-    printf("Hello world! argc=%d", argc);
+// Функция изменения размеров окна с сохранением пропорций
+void changeSize(int w, int h)
+{
+    // предотвращение деления на ноль
+    if (h == 0)
+    {
+        h = 1;
+    }
+    float ratio = w * 1.0 / h;
+
+    // используем матрицу проекции
+    glMatrixMode(GL_PROJECTION);
+
+    // обнуляем матрицу
+    glLoadIdentity();
+
+    // установить параметры вьюпорта
+    glViewport(0, 0, w, h);
+
+    // установить корректную перспективу
+    gluPerspective(45.0f, ratio, 0.1f, 100.0f);
+
+    // вернуться к матрице проекции
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void renderScene(void)
+{
+    // очистить буфер цвета и глубины
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // обнулить трансформацию
+    glLoadIdentity();
+
+    // Здесь основной алгоритм рисования
+
+    // Обновить изображение
+    glutSwapBuffers();
+}
+
+void processNormalKeys(unsigned char key, int xx, int yy)
+{
+    // Выход по Esc
+    if (key == 27) {
+        exit(0);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    // Инициализация GLUT и создание окна
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Визуализация сортировки");
+
+    // Функции рисования
+    glutDisplayFunc(renderScene);
+    glutReshapeFunc(changeSize);
+    glutIdleFunc(renderScene);
+
+    // Функции работы с клавиатурой
+    glutIgnoreKeyRepeat(1);
+    glutKeyboardFunc(processNormalKeys);
+
+    // Главный цикл
+    glutMainLoop();
+
     return 0;
 }
