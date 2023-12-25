56b3ee92f90792e37a1a6c8a70e96ed56eaf2b13
Andrew Zaikov <andrew.zaikov@gmail.com>
2023-12-26 02:45:56 +0700

    bugfix animation

--- main.cpp
+++ main.cpp
@@ -11,21 +11,26 @@
 // идентификаторы меню
 int menu;
 
-// угол поворота камеры
+// Для движения и поворота камеры
+//  угол поворота камеры
 float angle=0.0;
-// координаты вектора направления движения камеры
+//  координаты вектора направления движения камеры
 float lx=0.0f, lz=-1.0f;
-// XZ позиция камеры
+//  XZ позиция камеры
 float x=0.0f, z=5.0f;
-// Движение камеры. Переменные инициализируются нулевыми значениями когда клавиши не нажаты
+//  Движение камеры. Переменные инициализируются нулевыми значениями когда клавиши не нажаты
 float deltaAngle = 0.0f;
 float deltaMove = 0;
 
+// Отладочная надпись - текст
+char statusText[100] = {'\0'};
+// Размеры окна
 int width, height;
 
-char statusText[100] = {'\0'};
+// Массив шариков заполнен?
+bool arrayFilled = false;
 
-// Для удобства задания позиции
+// Структура (класс) - положение 
 struct Position {
     double x;
     double y;
@@ -40,7 +45,22 @@ struct Position {
     }
 };
 
-// Для удобства задания цвета
+// Структура (класс) - движение
+struct Move {
+    int item;
+    Position start = Position();
+    Position stop = Position();
+
+    Move(){}
+
+    Move(int item, Position start, Position stop) {
+        this->item = item;
+        this->start = start;
+        this->stop = stop;
+    }
+};
+
+// Структура (класс) - цвет
 struct Color {
     double r;
     double g;
@@ -55,7 +75,7 @@ struct Color {
     }
 };
 
-// Шарик
+// Структура (класс) - Шарик
 struct Circle {
     Position pos = Position();
     Color clr = Color();
@@ -88,9 +108,27 @@ const Color ORANGE = Color(1, 0.75f, 0);
 
 // массив с цветами
 const Color colors[] = {RED, ORANGE, YELLOW, GREEN, SKY, BLUE, PINK, WHITE, GREY, DARK};
+
+// массив с шариками
 Circle circles[10];
 
-// Нарисовать надпись
+// массив с перемещениями
+Move moves[100];
+
+// количество перемещений
+int moveCount = -1;
+
+// Дельта по высоте
+double dH = 2.0f;
+
+// Число шагов для анимации
+double nSteps = 50;
+
+// перемещение по X, Y
+double deltaX = 0.0f, deltaY = 0.0f;
+
+
+// Нарисовать подпись
 void renderStrokeFontString(Position pos, void *font, char *string) {  
 	glPushMatrix();
 	glTranslatef(pos.x, pos.y, pos.z);
@@ -101,6 +139,7 @@ void renderStrokeFontString(Position pos, void *font, char *string) {
 	glPopMatrix();
 }
 
+// Нарисовать шарик без подписи 
 void renderCircleAt(Position pos, double rad, Color clr) {
     glPushMatrix(); // сохраняем текущие координаты
 
@@ -111,17 +150,17 @@ void renderCircleAt(Position pos, double rad, Color clr) {
     glPopMatrix();
 }
 
+// Нарисовать шарик с подписью 
 void renderCircle(Circle circle) {
     renderCircleAt(circle.pos, circle.r, circle.clr);
     Position capPos = Position(circle.pos.x-0.06*strlen(circle.name), circle.pos.y+circle.r+0.02f, circle.pos.z);
     renderStrokeFontString(capPos, GLUT_STROKE_ROMAN, circle.name);
 }
 
+// Нарисовать 10 шариков
 void renderCircles()
 {
-
     for (int i=0; i<10; i++) {
-        circles[i] = Circle(Position(-8.0f+1.5f*i, 1.0f, -5.0f), 0.1f+0.05f*i, colors[i], i);
         renderCircle(circles[i]);
     }
 }
@@ -155,7 +194,7 @@ void changeSize(int w, int h)
     glMatrixMode(GL_MODELVIEW);
 }
 
-// Перейти в ортогональную проекцию
+// Отладочная надпись - перейти в ортогональную проекцию
 void setOrthographicProjection() {
 	// переключения режима проецирования
 	glMatrixMode(GL_PROJECTION);
@@ -173,7 +212,7 @@ void setOrthographicProjection() {
 	glMatrixMode(GL_MODELVIEW);
 }
 
-// Вернуться в перспективную проекцию
+// Отладочная надпись - вернуться в перспективную проекцию
 void restorePerspectiveProjection() {
 	glMatrixMode(GL_PROJECTION);
 
@@ -184,7 +223,7 @@ void restorePerspectiveProjection() {
 	glMatrixMode(GL_MODELVIEW);
 }
 
-// Вывести надпись 
+// Отладочная надпись - вывести надпись 
 void renderBitmapString(Position pos, void *font, char *string) {
 	glRasterPos3f(pos.x, pos.y, pos.z);
 
@@ -193,18 +232,87 @@ void renderBitmapString(Position pos, void *font, char *string) {
 	}
 }
 
+// для движения камеры - перемещение
 void computePos(float deltaMove) 
 {
 	x += deltaMove * lx * 0.1f;
 	z += deltaMove * lz * 0.1f;
 }
- 
+
+// для движения камеры - поворот
 void computeDir(float deltaAngle)
 {
 	angle += deltaAngle;
 	lx = sin(angle);
 	lz = -cos(angle);
 }
+
+// Заполнить массив с шариками случайными значениями
+void randomizeArray() {
+    int k;
+
+    for (int i=0; i<10; i++) {
+        k = random()%10;
+        circles[i] = Circle(Position(-8.0f+1.5f*i, 1.0f, -5.0f), 0.1f+0.05f*k, colors[i], 10*k+i);
+    }
+}
+
+// Для теста: анимация, изменить 0 и 3 элементы
+void swapElements(int l, int r) {
+    // записать в обратном порядке
+    Position lpos = circles[l].pos;
+    Position rpos = circles[r].pos;
+
+    // левую на место правой
+    moves[++moveCount] = Move(l, Position((lpos.x+rpos.x)/2.0f, lpos.y - dH, lpos.z), rpos);
+    // правую на место левой
+    moves[++moveCount] = Move(r, Position((lpos.x+rpos.x)/2.0f, rpos.y + dH, rpos.z), lpos);
+    // левую на середину внизу
+    moves[++moveCount] = Move(l, lpos, Position((lpos.x+rpos.x)/2.0f, lpos.y - dH, lpos.z));
+    // правую на середину вверху
+    moves[++moveCount] = Move(r, rpos, Position((lpos.x+rpos.x)/2.0f, rpos.y + dH, rpos.z));
+}
+
+// Рассчитать следующее движение
+void computeNextMove() {
+    if (moveCount < 0) {
+        return;
+    }
+
+    Move move = moves[moveCount];
+    printf("move.item = %d, moveCount = %d\n", move.item, moveCount);
+    if (deltaX == 0 && deltaY == 0) {
+        deltaX = (move.stop.x - move.start.x) / nSteps;
+        deltaY = (move.stop.y - move.start.y) / nSteps;
+
+        printf("circles[move.item].pos.x = %f, circles[move.item].pos.y = %f\n", circles[move.item].pos.x, circles[move.item].pos.y);
+        printf("move.stop.x = %f, move.start.x = %f\n", move.stop.x, move.start.x);
+        printf("move.stop.y = %f, move.start.y = %f\n", move.stop.y, move.start.y);
+        printf("deltaX = %f, deltaY = %f\n", deltaX, deltaY);
+    }
+
+    Position pos = circles[move.item].pos;
+    printf("current pos.x = %f, pos.y = %f, deltaX = %f, deltaY = %f\n", pos.x, pos.y, deltaX, deltaY);
+
+    pos.x += deltaX;
+    if (abs(pos.x - move.stop.x) <= abs(deltaX)) {
+        pos.x = move.stop.x;
+        deltaX = 0;
+    }
+
+    pos.y += deltaY;
+    if (abs(pos.y - move.stop.y) <= abs(deltaY)) {
+        pos.y = move.stop.y;
+        deltaY = 0;
+    }
+    printf("new pos.x = %f, pos.y = %f, dx = %f, dy = %f\n", pos.x, pos.y, abs(pos.x - move.stop.x), abs(pos.y - move.stop.y));
+    circles[move.item].pos = pos;
+
+    if (deltaX == 0 && deltaY == 0) {
+        moveCount--;
+    }
+}
+
 
 // Функция создания изображения
 void renderScene()
@@ -215,6 +323,14 @@ void renderScene()
 	if (deltaAngle) {
 		computeDir(deltaAngle);
     }
+
+    if (!arrayFilled) {
+        arrayFilled = true;
+        srand(glutGet(GLUT_ELAPSED_TIME));
+        randomizeArray();
+    }
+
+    computeNextMove();
 
     // очистить буфер цвета и глубины
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -263,6 +379,7 @@ void processNormalKeys(unsigned char key, int x, int y)
     }
 }
 
+// для движения камеры - нажатие кнопки
 void pressKey(int key, int x, int y) {
  
 	switch (key) {
@@ -280,7 +397,8 @@ void pressKey(int key, int x, int y) {
 			break;
 	}
 }
- 
+
+// для движения камеры - отпустить кнопку
 void releaseKey(int key, int x, int y) {
  
 	switch (key) {
@@ -296,10 +414,11 @@ void releaseKey(int key, int x, int y) {
 }
 
 void processMenu(int option) {
- 
+
 	switch (option) {
 		case 1: // randomize
             strcpy(statusText, "Randomize array");
+            randomizeArray();
 			break;
 		case 2: // insertion
             strcpy(statusText, "Insertion sort");
@@ -307,15 +426,20 @@ void processMenu(int option) {
 		case 3: // bubble
             strcpy(statusText, "Bubble sort");
 			break;
+		case 4: // Swap 0 and 3
+            strcpy(statusText, "Swap 0 and 3");
+            swapElements(0, 3);
+			break;
 	}
 }
 
 void createPopupMenus() {
  
 	menu = glutCreateMenu(processMenu);
-	glutAddMenuEntry("Randomize array",1 );
-	glutAddMenuEntry("Insertion sort",2 );
-	glutAddMenuEntry("Bubble sort",3 );
+	glutAddMenuEntry("Randomize array", 1);
+	glutAddMenuEntry("Insertion sort", 2);
+	glutAddMenuEntry("Bubble sort", 3);
+	glutAddMenuEntry("Move 0 and 3", 4);
 
 	// прикрепить меню к правой кнопке
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
@@ -338,7 +462,8 @@ int main(int argc, char **argv)
     // Функции работы с клавиатурой
     glutIgnoreKeyRepeat(1);
     glutKeyboardFunc(processNormalKeys);
-	glutIdleFunc(renderScene);
+
+    // для движения камеры
 	glutSpecialFunc(pressKey);
 	glutSpecialUpFunc(releaseKey);
 
