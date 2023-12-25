85c19a91056c48e730b0175de35c09ec209e5d04
Andrew Zaikov <andrew.zaikov@gmail.com>
2023-12-25 21:37:09 +0700

    captions

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #ifdef __APPLE__
 #include <GLUT/glut.h>
@@ -19,6 +21,8 @@ struct Position {
     double y;
     double z;
 
+    Position(){}
+
     Position(double x, double y, double z) {
         this->x = x;
         this->y = y;
@@ -32,12 +36,32 @@ struct Color {
     double g;
     double b;
 
+    Color(){}
+
     Color(double r, double g, double b) {
         this->r = r;
         this->g = g;
         this->b = b;
     }
 };
+
+// Шарик
+struct Circle {
+    Position pos = Position();
+    Color clr = Color();
+    double r;
+    char name[5];
+
+    Circle(){}
+
+    Circle(Position pos, double r, Color clr, int num) {
+        this->pos = pos;
+        this->clr = clr;
+        this->r = r;
+        sprintf(this->name, "%d", num);
+    }
+};
+
 
 // Цветовые константы
 const Color RED = Color(1.0f, 0, 0);
@@ -52,6 +76,16 @@ void shiftColors()
 {
 }
 
+void renderStrokeFontString(Position pos, void *font, char *string) {  
+	glPushMatrix();
+	glTranslatef(pos.x, pos.y, pos.z);
+	glScalef(0.002f, 0.002f, 0.002f);
+	for (char *c=string; *c != '\0'; c++) {
+		glutStrokeCharacter(font, *c);
+	}
+	glPopMatrix();
+}
+
 void renderCircleAt(Position pos, double rad, Color clr) {
     glPushMatrix(); // сохраняем текущие координаты
 
@@ -62,14 +96,25 @@ void renderCircleAt(Position pos, double rad, Color clr) {
     glPopMatrix();
 }
 
+void renderCircle(Circle circle) {
+    renderCircleAt(circle.pos, circle.r, circle.clr);
+    Position capPos = Position(circle.pos.x-0.06*strlen(circle.name), circle.pos.y+circle.r+0.02f, circle.pos.z);
+    renderStrokeFontString(capPos, GLUT_STROKE_ROMAN, circle.name);
+}
+
 void renderCircles()
 {
     shiftColors();
 
-    renderCircleAt(Position(-1.0f, 1.0f, -5.0f), 0.2f, RED);
-    renderCircleAt(Position(1.0f, 1.0f, -5.0f), 0.2f, GREEN);
-    renderCircleAt(Position(-1.0f, -1.0f, -5.0f), 0.2f, BLUE);
-    renderCircleAt(Position(1.0f, -1.0f, -5.0f), 0.2f, WHITE);
+    Circle ball1 = Circle(Position(-1.0f, 1.0f, -5.0f), 0.2f, RED, 1);
+    Circle ball2 = Circle(Position(1.0f, 1.0f, -5.0f), 0.3f, GREEN, 2);
+    Circle ball3 = Circle(Position(-1.0f, -1.0f, -5.0f), 0.1f, BLUE, 3);
+    Circle ball4 = Circle(Position(1.0f, -1.0f, -5.0f), 0.5f, WHITE, 4);
+
+    renderCircle(ball1);
+    renderCircle(ball2);
+    renderCircle(ball3);
+    renderCircle(ball4);
 }
 
 // Функция изменения размеров окна с сохранением пропорций
@@ -103,15 +148,18 @@ void changeSize(int w, int h)
 
 // Перейти в ортогональную проекцию
 void setOrthographicProjection() {
-	//переключения режима проецирования
+	// переключения режима проецирования
 	glMatrixMode(GL_PROJECTION);
-	//Сохраняем предыдущую матрицу, которая содержит
-        //параметры перспективной проекции
+
+	// Сохраняем предыдущую матрицу, которая содержит параметры перспективной проекции
 	glPushMatrix();
-	//обнуляем матрицу
+
+	// обнуляем матрицу
 	glLoadIdentity();
-	//устанавливаем 2D ортогональную проекцию
+
+	// устанавливаем 2D ортогональную проекцию
 	gluOrtho2D(0, width, height, 0);
+
 	// возврата в режим обзора модели
 	glMatrixMode(GL_MODELVIEW);
 }
@@ -119,30 +167,26 @@ void setOrthographicProjection() {
 // Вернуться в перспективную проекцию
 void restorePerspectiveProjection() {
 	glMatrixMode(GL_PROJECTION);
-	//восстановить предыдущую матрицу проекции
+
+	// восстановить предыдущую матрицу проекции
 	glPopMatrix();
-	//вернуться в режим модели
+
+	// вернуться в режим модели
 	glMatrixMode(GL_MODELVIEW);
 }
 
 // Вывести надпись 
-void renderBitmapString(
-		float x,
-		float y,
-		float z,
-		void *font,
-		char *string) {
- 
-	char *c;
-	glRasterPos3f(x, y, z);
-	for (c=string; *c != '\0'; c++) {
+void renderBitmapString(Position pos, void *font, char *string) {
+	glRasterPos3f(pos.x, pos.y, pos.z);
+
+	for (char *c=string; *c != '\0'; c++) {
 		glutBitmapCharacter(font, *c);
 	}
 }
 
 
 // Функция создания изображения
-void renderScene(void)
+void renderScene()
 {
     // очистить буфер цвета и глубины
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -171,7 +215,7 @@ void renderScene(void)
     setOrthographicProjection();
 	    glPushMatrix();
 	        glLoadIdentity();
-	        renderBitmapString(20, 30, 0, GLUT_BITMAP_HELVETICA_18, "Some text");
+	        renderBitmapString(Position(20, 30, 0), GLUT_BITMAP_HELVETICA_18, "Some text");
 	    glPopMatrix();
 	restorePerspectiveProjection();
 
